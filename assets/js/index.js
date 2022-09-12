4e92a90339ec1b651a3061c60366965340baa2d3
itinvader <itinvader@2425457386@qq.com>
2022-09-12 17:28:47 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+$(function () {
+  getUserInfo();
+  function getUserInfo() {
+    $.ajax({
+      method: "GET",
+      url: "/my/userinfo",
+      headers: {
+        Authorization: localStorage.getItem("token") || "",
+      },
+      success: function (res) {
+        console.log(res);
+      },
+    });
+  }
+});
